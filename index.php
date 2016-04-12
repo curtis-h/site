a4d1ff18808bbbc2c55ae0b7c7fe942a2810c7b7
Curtis H <ch@curtish.me>
2016-04-12 21:14:58 +0100

    Adding site menu and initial styles

--- index.php
+++ index.php
@@ -4,7 +4,29 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Curtis Harding</title>
+    <link rel="stylesheet" type="text/css" href="/assets/style.css"></link>
+    <script type="text/js" src="/assets/app.js"></script>
 </head>
 <body>
-    </body>
+    <section class="sidebar">
+        <header class="sidebar-top">
+            <h1>Curtis<br>Harding</h1>
+        </header>
+        
+        <nav class="sidebar-bottom">
+            <a class="red" href="#about">About</a>
+            <a class="orange" href="#tech">Tech</a>
+            <a class="yellow" href="#hackference">Hackference</a>
+            <a class="green" href="#urbangames">Urban Games</a>
+            <a class="blue" href="#d&d">D&amp;D</a>
+            <a class="purple" href="#mtb">Mtb</a>
+        </nav>
+        
+        <footer>
+            <a class="icon twitter" href="https://twitter.com/curtis_h"></a>
+            <a class="icon github" href="https://github.com/curtis-h"></a>
+            <a class="icon gmail" href="mailto:ch@curtish.me"></a>
+        </footer>
+    </section>
+</body>
 </html>
