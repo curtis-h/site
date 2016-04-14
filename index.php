9481a3d4f0100182ee20c3715cee85392019472a
Curtis H <ch@curtish.me>
2016-04-14 19:25:01 +0100

    adjusting styles and adding copy to site

--- index.php
+++ index.php
@@ -30,11 +30,56 @@
     </section>
     
     <section class="content">
-        <header></header>
         <main>
+            <div>
+                <a name="about"></a>
+                <h2>About</h2>
+                <p>Hi, I'm Curtis, a person who enjoys coding, mountain biking, urban gaming, airsofting, boardgaming, hackathon-ing amongst many other things. This is a site about me and the things I enjoy. I'll try to keep things up to date, but no promises.</p>
+                <p>Currently I work as a full stack developer at <a href="https://www.i-escape.com">i-escape</a> during the day, and on various side projects during the night, you can read more about my technical endeavours <a href="#tech">here</a>.</p>
+                <p>I'm one of the organisers of <a href="#hackference">Hackference</a>, a yearly tech conference and hackathon in Birmingham, currently in it's fourth year and looking to be better than ever.</p>
+                <p>I also run <a href="#urbangames">Bristol Urban Games</a>. A monthly meetup in the centre of Bristol where we provide a gamified reason for people to meet, run about and have fun.</p>
+                <p>I DM a weekly D&D group for my friends, which is great fun. You can read about their hilarious exploits <a href="#d&d">here</a>.</p>
+            </div>
             
+            <div>
+                <a name="tech"></a>
+                <h2>Tech</h2>
+                <p>I enjoy just about everything tech related, with my passions being development and security.</p>
+                <p>My core skills include PHP, Javascript, MySQL, bash and RHEL as these are what I use in work. For hacks and personal projects I always try to use different tech and new things and as such have played with and am competent with a much larger group of technologies.</p>
+                <p>On top of that I have played with a lot of tech and api's, either in hackathons or personal projects, a small list of these would be: </p>
+            </div>
+            
+            <div>
+                <a name="hackference"></a>
+                <h2>Hackference</h2>
+                <p>
+                    Is a 3 day event focused on inspiring people to learn and build. 
+                    Hackference consists of a single day two track conference, and a two day hackathon.
+                    This is our fourth year
+                </p>
+            </div>
+            
+            <div>
+                <a name="urbangames"></a>
+                <h2>Urban Games</h2>
+                <p>
+                    I run a monthly meetup group called <a href="https://www.facebook.com/bristolurbangames" target="_blank">Bristol Urban Games</a>.
+                    Our games involve a mixture of running and puzzle solving and are a great way to have some fun outside once a month.
+                </p>
+            </div>
+            
+            <div>
+                <a name="d&d"></a>
+                <h2>D&amp;D</h2>
+                <p></p>
+            </div>
+            
+            <div>
+                <a name="mtb"></a>
+                <h2>Mtb</h2>
+                <p>I like riding bikes</p>
+            </div>
         </main>
-        <footer></footer>
     </section>
 </body>
 </html>
